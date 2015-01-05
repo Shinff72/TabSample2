73b82b6e945fb54d39b0621b1def3c543de42821
Shinichi Furusawa <furusawa@ohako-inc.jp>
2015-01-05 22:20:01 +0900

    Initial Commit

---
+++ TabSample2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabSample2
+//
+//  Created by 古澤 伸一 on 2015/01/05.
+//  Copyright (c) 2015年 古澤 伸一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabSample2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabSample2
+//
+//  Created by 古澤 伸一 on 2015/01/05.
+//  Copyright (c) 2015年 古澤 伸一. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
